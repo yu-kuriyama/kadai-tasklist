bc40d8c60231965b7526766d4e5ede12dd2a2f67
yuu <speedyuu@gmail.com>
2019-08-26 06:58:10 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
@@ -11,7 +12,6 @@ class UsersController < ApplicationController
   end
 
   def create
-      def create
     @user = User.new(user_params)
 
     if @user.save
@@ -27,6 +27,5 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
      root to: 'tasks#index'
     resources :tasks
+    
+      get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
     
      get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]

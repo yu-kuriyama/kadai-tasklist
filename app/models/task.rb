34bc6f6c0f70194626c0768ab9f6807009d2f023
yuu <speedyuu@gmail.com>
2019-09-06 08:07:16 +0000

    for heroku

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,5 @@ class Task < ApplicationRecord
     belongs_to :user
     
     validates :content, presence: true, length: { maximum: 255 }
-    validates :title, presence: true, length: { maximum: 255 }
+    
 end

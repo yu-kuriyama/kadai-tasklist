67ed7b22f41e0ce89cefb6095fc135ec769f6307
yuu <speedyuu@gmail.com>
2019-09-09 10:53:36 +0000

    add :show before_action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,17 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
-    before_action :correct_user, only: [:destroy]
+    before_action :correct_user, only: [:destroy, :show]
     
     def index
         if logged_in?
-      @task = current_user.tasks.build  # form_with 用
-      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-    end
+          @task = current_user.tasks.build  # form_with 用
+          @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+        end
         #@tasks = Task.all.page(params[:page])
     end
     
     def show
+
         @task = Task.find(params[:id])
     end
     
@@ -60,5 +61,12 @@ class TasksController < ApplicationController
     def task_params
         params.require(:task).permit(:content, :status)
     end    
+    
+     def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+        redirect_to root_url
+        end   
+     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
     
      get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+  resources :tasks, only: [:create, :destroy]
     
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
